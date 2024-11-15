14a7de4071497a786050dc0fbf6fbfd79c100c92
RehanFarooq <bscs22076@itu.edu.pk>
2024-11-15 07:45:33 +0500

    header footer and homepage

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
